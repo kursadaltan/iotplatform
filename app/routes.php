e6d575eee1af523841c6bc1dabfd9af79daeb802
kursadaltan <iletisim@kursadaltan.com>
2021-06-10 11:24:41 +0300

    Updates

--- app/routes.php
+++ app/routes.php
@@ -24,8 +24,7 @@ return function(App $app){
     
     $app->post("/api/v1/login", SessionController::class.':login');
     $app->group('/api/v1', function (RouteCollectorProxy $group) {
-        $group->get('/users',   UserController::class.':getUsers');
-        $group->get('/users/{user}',   UserController::class.':getUser');
+        $group->get('/users[/{user:[0-9]+}]',   UserController::class.':getUsers');        
         $group->delete('/users/{user}',   UserController::class.':deleteUser');
         $group->put('/users/{user}',   UserController::class.':updateUser');
         $group->post('/users/new',   UserController::class.':newUser');
@@ -38,14 +37,12 @@ return function(App $app){
         $group->delete('/groups/{group}',   UserController::class.':deleteGroup');
         $group->get('/groups/{group}/users', UserController::class.':getGroupUsers');
 
-        $group->get('/modems', ModemController::class.':getModems');
-        $group->get('/modems/{page}', ModemController::class.':getModems');
+        $group->get('/modems[/{page:[0-9]+}]', ModemController::class.':getModems');
         $group->post('/modems/new', ModemController::class.':newModem');
         $group->put('/modem/{modem}', ModemController::class.':updateModem');
         $group->delete('/modem/{modem}', ModemController::class.':deleteModem');
 
-        $group->get('/devices', DeviceController::class.':getDevices');
-        $group->get('/devices/{page}', DeviceController::class.':getDevices');
+        $group->get('/devices[/{page:[0-9]+}]', DeviceController::class.':getDevices');     
         $group->get('/device/table/{device}', DeviceController::class.':getDeviceTable');
         $group->post('/device/new', DeviceController::class.':newDevice');
         $group->put('/device/{device}', DeviceController::class.':updateDevice');
@@ -54,8 +51,7 @@ return function(App $app){
         $group->put('/device/table/{table}', DeviceController::class.':updateDeviceTable');
         $group->delete('/device/table/{table}', DeviceController::class.':deleteDeviceTable');
 
-        $group->get('/products', ProductController::class.':getProducts');
-        $group->get('/products/{page}', ProductController::class.':getProducts');
+        $group->get('/products[/{page:[0-9]+}]', ProductController::class.':getProducts');    
         $group->get('/product/table/{product}', ProductController::class.':getProductTable');
         $group->post('/product/new', ProductController::class.':newProduct');
         $group->put('/product/{product}', ProductController::class.':updateProduct');
@@ -64,7 +60,15 @@ return function(App $app){
         $group->put('/product/table/{table}', ProductController::class.':updateProductTable');
         $group->delete('/product/table/{table}', ProductController::class.':deleteProductTable');
 
+        $group->get('/products/groups', ProductController::class.':getProductGroups');
+        $group->post('/products/groups/new', ProductController::class.':newProductGroup');
+        $group->put('/products/groups/{group}', ProductController::class.':updateProductGroup');
+        $group->delete('/products/groups/{group}', ProductController::class.':deleteProductGroup');
 
+        $group->get('/products/labels', ProductController::class.':getLabels');
+        $group->post('/products/labels/new', ProductController::class.':newLabel');
+
+        
 
     })->add(new AuthMiddleware());
 
